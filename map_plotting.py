5d9d2131a5dafa0787ca35254be1782839b0af6f
Yeo Zheng Quan <zqyeo11@gmail.com>
2023-05-01 13:16:27 +1000

    Commit

--- map_plotting.py
+++ map_plotting.py
@@ -22,8 +22,47 @@ def plot_itinerary(itinerary: Itinerary, projection = 'robin', line_width=2, col
     :param line_width: The width of the line to draw.
     :param colour: The colour of the line to draw.
     """
-    #TODO
+    
+    # Bounding box
+    #min_lat, max_lat, min_lon, max_lon = 5,5,5,5
 
+    min_lat, max_lat, min_lon, max_lon = float('inf'), -float('inf'), float('inf'), -float('inf')
+    for city in city_list:
+        lat, lon = city.coordinates
+        if lat < min_lat:
+            min_lat = lat
+        if lat > max_lat:
+            max_lat = lat
+        if lon < min_lon:
+            min_lon = lon
+        if lon > max_lon:
+            max_lon = lon
+
+    map_size = max(max_lat - min_lat, max_lon - min_lon, 50)
+
+    padding = 5
+    min_lat = min_lat - padding if min_lat > -90 + map_size / 2 else -90 + map_size / 2
+    max_lat = max_lat + padding if max_lat < 90 - map_size / 2 else 90 - map_size / 2
+    min_lon = min_lon - padding if min_lon > -180 + map_size / 2 else -180 + map_size / 2
+    max_lon = max_lon + padding if max_lon < 180 - map_size / 2 else 180 - map_size / 2
+
+    # Setting up the map
+    m = Basemap(projection=projection, lat_0=0,lon_0=0,resolution="l",
+                llcrnrlon=min_lon, llcrnrlat=min_lat, urcrnrlon=max_lon, urcrnrlat=max_lat)
+
+    m.drawcoastlines(color='k', linewidth=0.5)
+    m.fillcontinents(color='#c0c0c0')
+
+    cities = city_list
+    for i in range(len(cities) - 1):
+        lat1, lon1 = cities[i].coordinates
+        lat2, lon2 = cities[i+1].coordinates
+        m.drawgreatcircle(lon1, lat1, lon2, lat2, linewidth=line_width, color=colour)
+
+    filename = f"map_{'_'.join(c.name for c in city_list)}.png".replace(" ", "_")
+    plt.savefig(filename, dpi=200)
+    print(f"Map saved to {filename}")
+    plt.show()
 
 if __name__ == "__main__":
     # create some cities
