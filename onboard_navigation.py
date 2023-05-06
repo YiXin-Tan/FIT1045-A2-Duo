103b37b19fbe96e74b9d5e5bdb53d14a7dc58e4f
YiXin-Tan <yixin360@gmail.com>
2023-05-06 13:54:35 +1000

    removed select country by first character, added docstrings placeholders

--- onboard_navigation.py
+++ onboard_navigation.py
@@ -6,9 +6,9 @@ It puts together all parts of the assignment.
 @file onboard_navigation.py
 """
 import os
-from vehicles import Vehicle, create_example_vehicles
+from vehicles import Vehicle, create_example_vehicles, CrappyCrepeCar, DiplomacyDonutDinghy, TeleportingTarteTrolley
 from city import City
-from country import Country
+from country import Country, find_country_of_city
 from csv_parsing import create_cities_countries_from_csv
 from path_finding import find_shortest_path
 from map_plotting import plot_itinerary
@@ -31,42 +31,75 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def validate_input(prompt: str, valid_inputs: list) -> int:
+def validate_input(prompt: str, valid_instances: list):  # -> Vehicle|Country|City
     """
+    Generates selection user interface.
+    Repeatedly ask user for input until they enter an input
+    within a set valid of options.
 
-    :param prompt:
-    :param valid_inputs:
-    :return: validated index of the valid_input
+    :param prompt: The prompt to display to the user
+    :param valid_instances: A list of values to accept. Contains all vehicle, country or city .
+    :return: validated instance within the given valid_instances list
     """
-    # TODO: cant input same city
-    valid_user_options = [str(i) for i in range(0, len(valid_inputs))]
-    for i in range(0, len(valid_inputs)):
-        prompt += f'\n {i}. {valid_inputs[i]}'
-    prompt += f'\nPlease enter a number [0-{len(valid_inputs) - 1}]:'
+    if isinstance(valid_instances[0], City):
+        # For ZQ,
+        # if valid_instances are all city_instance, it will print out melbourne (1237), which is not pretty
+        # valid_instances = [melb_obj,         kl_obj,    baoding_obj]
+        # original __str__= [melbourne (1237), kl (4567), baoding (234544)]
+        # thus we shorten it by getting city_instance.name
+        # original.name   = [melbourne,        kl,        baoding]
+        valid_names = [city.name for city in valid_instances]  # shorten __str__ of city instance
+    else:
+        valid_names = valid_instances  # retain __str__ of vehicle and country instance
+
+    # allow users to only input whole numbers
+    valid_user_options = [str(i) for i in range(0, len(valid_instances))]
+    for i in range(0, len(valid_instances)):
+        prompt += f'\n {i}. {valid_names[i]}'
+    prompt += f'\nPlease enter a number [0-{len(valid_instances) - 1}]:'
 
     while True:
         try:
             user_option = input(prompt)
             assert user_option in valid_user_options, 'User option is not in valid inputs'
             clear_screen()
-            return int(user_option)  # terminates function and breaks from while loop
+            return valid_instances[int(user_option)]  # return vehicle/city/country instance, terminates function and breaks from while loop
         except AssertionError:
             clear_screen()
             print('Invalid input, input must be an integer and within range, please try again')
             continue
 
-    # return int(user_option)
 
+def create_vehicles() -> [Vehicle]:
+    """
 
-def select_vehicle() -> Vehicle:
-    vehicle_instances = create_example_vehicles()
+    :return:
+    """
+    ccc = CrappyCrepeCar(200)
+    ddd = DiplomacyDonutDinghy(100, 500)
+    ttt = TeleportingTarteTrolley(3, 2000)
+    return [ccc, ddd, ttt]
+
+def select_vehicle(vehicles: [Vehicle]) -> Vehicle:
+    vehicles_description = f'''
+    CrappyCrepeCar can travel between any cities in the world at a {vehicles[0].speed} 
+    DiplomacyDonutDinghy can travel between any two cities in the same country, or between international "primary" cities
+    TeleportingTarteTrolley can travel between any two cities if the distance is less than a given maximum distance, in a fixed time.
+'''
+
     #TODO: add vehicle description
-    vehicle_idx = validate_input('Select a vehicle', vehicle_instances)
-    return vehicle_instances[vehicle_idx]
+    #TODO: print selected vehicle
+    vehicle = validate_input('Select a delivery vehicle', vehicles)
+    return vehicle
 
 
 def validate_country(country_names: [str], origin_dest: str) -> [Country]:
-    az = ['a', ' b', ' c', ' d', ' e', ' f', ' g', ' h', ' i', ' j', ' k', ' l', ' m', ' n', ' o', ' p', ' q', ' r', ' s', ' t', ' u', ' v', ' w', ' x', ' y', ' z']
+    """
+    NOT IN USE
+    :param country_names:
+    :param origin_dest:
+    :return:
+    """
     user_option = None
     filtered_country_names = []
     while True:  # while filtered_country_names != None
@@ -98,21 +131,6 @@ def validate_country(country_names: [str], origin_dest: str) -> [Country]:
     #             filtered_country_names.append(country_name)
     # print(filtered_country_names)
 
-
-def validate_city(country: Country) -> City:
-    pass
-
-
-def validate_dest_city(origin_city_name: str, dest_city_names: [str]):
-
-    dest_city_name = origin_city_name  # initialise
-
-    while dest_city_name == origin_city_name:
-        dest_city_idx = validate_input('Select the destination city', dest_city_names)
-        dest_city_name = dest_city_names[dest_city_idx]
-
-    return dest_city_idx
-
 def select_origin_and_destination_cities() -> (City, City):
     """
     Select origin city from a list of countries
@@ -120,40 +138,46 @@ def select_origin_and_destination_cities() -> (City, City):
     :return: A list of the origin and destination city
     """
     create_cities_countries_from_csv("worldcities_truncated.csv")
-    country_instances = Country.name_to_countries
-    country_names = list(Country.name_to_countries.keys())
+    countries = list(Country.name_to_countries.values())
 
-    origin_country_idx = validate_input("Which country is the destination city in?", country_names)
-    origin_country_name = country_names[origin_country_idx]
-    origin_country_instance = Country.name_to_countries[origin_country_name]
+    origin_country = validate_input("Which country is the origin city in?", countries)
+    origin_city = validate_input("Which is the origin city?", origin_country.cities)
 
-    origin_city_instances = origin_country_instance.cities
-    origin_city_names = [city.name for city in origin_city_instances]
+    dest_country = validate_input("Which country is the destination city in?", countries)
+    dest_city = origin_city
+    while dest_city == origin_city:  # TODO: check same city
+        dest_city = validate_input("which is the destination city?", dest_country.cities)
 
-    origin_city_idx = validate_input('Select the origin city', origin_city_names)
-    origin_city_instance = origin_city_instances[origin_city_idx]
-    print(origin_city_instance, origin_country_instance)
+    # print(f'Origin city: {origin_city.name} {origin_country}')
+    # print(f'Destination city: {dest_city.name} {dest_country}')
 
-    dest_country_idx = validate_input('Which country is the destination city in?', country_names)
-    dest_country_name = country_names[dest_country_idx]
-    dest_country_instance = Country.name_to_countries[dest_country_name]
-    dest_city_instances = dest_country_instance.cities
-    dest_city_names = [city.name for city in dest_city_instances]
-    dest_city_idx = validate_input('Select the destination city', dest_city_names)
-    # TODO: check same city
-    dest_city_instance = dest_city_instances[dest_city_idx]
-
-    print(f'Origin city: {origin_city_instance.name} {origin_country_instance}')
-    print(f'Destination city: {dest_city_instance.name} {dest_country_instance}')
+    return origin_city, dest_city
 
 
-    return origin_city_instance, dest_city_instance
+def display_vehicle_and_cities(vehicle: Vehicle, origin_city: City, dest_city: City) -> None:
+    """
 
+    :param vehicle:
+    :param origin_city:
+    :param dest_city:
+    :return:
+    """
+    print(vehicle)
+    print(f'Origin city: {origin_city.name} {find_country_of_city(origin_city)}')
+    print(f'Destination city: {dest_city.name} {find_country_of_city(dest_city)}')
 
 def main():
-    vehicle = select_vehicle()
+    """
+
+    :return: None
+    """
+    print("===== Welcome to Papa Pierre's Pâtisseries =====")
+    vehicles = create_vehicles()
+    vehicle = select_vehicle(vehicles)
     origin_city, dest_city = select_origin_and_destination_cities()
     itinerary = find_shortest_path(vehicle=vehicle, from_city=origin_city, to_city=dest_city)
+    display_vehicle_and_cities(vehicle, origin_city, dest_city)
+
     if itinerary:
         print(itinerary)
         plot_itinerary(itinerary=itinerary)
@@ -167,4 +191,6 @@ def main():
 
 
 if __name__ == '__main__':
+    # TODO: add docstrings
+    # TODO: implement clear_screen
     main()
